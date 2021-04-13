63715b1be6289a9c8a20cc0a39f13c96a494fbcc
vcontreras <contrerastrece@gmail.com>
2021-04-13 13:32:41 -0500

    creacion de helper para peticiones AJAX 2/5 fake-api

---
+++ my-app-crud/src/helpers/helpersHttp.js
@@ -0,0 +1,61 @@
+export const helpHttp = () => {
+  const customFetch = (endpoint, options) => {
+    const defaultHeader = {
+      accept: "application/json",
+    };
+
+    // si la petición fetch no nos repsonde ya sea que el servidor esté caido por algún motivo
+    // abortaremos la petición con AbortController
+    const controller = new AbortController();
+    options.signal = controller.signal;
+
+    options.method = options.method || "GET";
+    options.header = options.headers
+      ? {
+          ...defaultHeader,
+          ...options.headers,
+        }
+      : defaultHeader;
+    options.body = JSON.stringify(options.body) || false;
+    if (!options.body) delete options.body;
+
+    console.log(options);
+    setTimeout(() => controller.abort(), 3000);
+
+    return fetch(endpoint, options)
+      .then((res) =>
+        res.ok
+          ? res.json()
+          : Promise.reject({
+              err: true,
+              status: res.status || "00",
+              statusText: res.statusText || "Ocurrio un error",
+            })
+      )
+      .catch((err) => err);
+  };
+
+  const get = (url, options = {}) => customFetch(url, options);
+
+  const post = (url, options = {}) => {
+    options.method = "POST";
+    return customFetch(url, options);
+  };
+
+  const put = (url, options = {}) => {
+    options.method = "PUT";
+    return customFetch(url, options);
+  };
+
+  const del = (url, options = {}) => {
+    options.method = "DELETE";
+    return customFetch(url, options);
+  };
+
+  return {
+    get,
+    post,
+    put,
+    del,
+  };
+};
