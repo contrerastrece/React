ac1e41bbe6562111ce25a45d02083fe93cb78dac
vcontreras <contrerastrece@gmail.com>
2021-04-13 17:31:55 -0500

    Implementando Loader y Message de errores 4/5

--- my-app-crud/src/components/CrudApi.js
+++ my-app-crud/src/components/CrudApi.js
@@ -2,25 +2,35 @@ import React, { useEffect, useState } from "react";
 import { helpHttp } from "../helpers/helpersHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 // sfc snippet
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   // cuando esté en Nulo haremos un Insert, y si  tiene un valor haremos un Update
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   let api = helpHttp();
-  let url = "http://localhost:5000/pokemons";
+  let url = "http://localhost:5000/pokemon";
 
   useEffect(() => {
+    setLoading(true);
     api.get(url).then((res) => {
-      // console.log(res);
+      console.log(res);
       // si no existe ningún error llenaremos nuestra db
       if(!res.err){
         setDb(res);
+        // asignamos el error a null
+        setError(null);
       }else{
         setDb(null);
+        // asignamos el error que llega en la variable res
+        setError(res);
       }
+      setLoading(false);
     });
   }, []);
 
@@ -61,11 +71,16 @@ const CrudApi = () => {
           dataToEdit={dataToEdit}
           setDataToEdit={setDataToEdit}
         ></CrudForm>
-        <CrudTable
+        {/* mostrará el Loader si aún no carga los datos */}
+        {loading && <Loader/>}
+        {/* si existe error cargará el componente */}
+        {error && <Message msg={`Error ${error.statusText}`} bgColor="#dc3545"/>}
+        {/* si no hay datos no cargará */}
+        {db && <CrudTable
           data={db}
           setDataToEdit={setDataToEdit}
           deleteData={deleteData}
-        ></CrudTable>
+        ></CrudTable>}
       </article>
     </>
   );
