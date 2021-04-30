31bc7329a947525a6e72291050204250e1f28241
vcontreras <contrerastrece@gmail.com>
2021-04-29 23:54:06 -0500

    select Anidados

--- my-app-crud/src/App.js
+++ my-app-crud/src/App.js
@@ -1,11 +1,16 @@
 import CRUDApp from "./components/CRUD-app";
 import CrudAPi from "./components/CrudApi";
+import SelectsAnidados from "./components/SelectsAnidados";
 import SongSearch from "./components/SongSearch";
 
 function App() {
   return (
    <div>     
      <h1>Ejercicios con React</h1>
+     <hr/>
+     <SelectsAnidados/>
+     <hr/>
+
      <SongSearch/>
      <hr/>
      <CrudAPi></CrudAPi>
---
+++ my-app-crud/src/components/SelectList.js
@@ -0,0 +1,13 @@
+const SelectList = () => {
+  return (
+    <div>
+      <h2>
+        <select name="" id="">
+          <option value="">---</option>
+        </select>
+      </h2>
+    </div>
+  );
+};
+
+export default SelectList;
