e163f0dd243ec3e044a303c1ea41c7e3d519a11c
vcontreras <contrerastrece@gmail.com>
2021-05-04 06:32:25 -0500

    programación de validaciones 3/4

--- my-app-crud/src/components/ContactForm.js
+++ my-app-crud/src/components/ContactForm.js
@@ -1,9 +1,50 @@
 import { useForm } from "../hooks/useForm";
 
-const initialForm = {};
+// inicializando con cadenas vacías
+const initialForm = {
+  name: "",
+  email: "",
+  subject: "",
+  comments: "",
+};
 
-const validationsForm = (form) => {};
+const validationsForm = (form) => {
+  let errors = {};
 
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
+
+  // si el cmapo nombre está vacio
+  if (!form.name.trim()) {
+    errors.name = "El campo nombre es requerido";
+  }else if(!regexName.test(form.name.trim())){
+    errors.name="El campo Nombre solo aceptan letras y espacios en blanco"
+  }
+  // si el cmapo nombre está vacio
+  if (!form.email.trim()) {
+    errors.email = "El campo Email es requerido";
+  }else if(!regexEmail.test(form.email.trim())){
+    errors.email="El campo Email solo aceptan email válidos"
+  }
+  // si el cmapo nombre está vacio
+  if (!form.subject.trim()) {
+    errors.subject = "El campo Asunto a tratar es requerido";
+  }
+  // si el cmapo nombre está vacio
+  if (!form.comments.trim()) {
+    errors.comments = "El campo Comentarios es requerido";
+  }else if(!regexComments.test(form.comments.trim())){
+    errors.comments="El campo Coments debe de contener 1-255 caracteres"
+  }
+
+  return errors;
+};
+
+let styles = {
+  fontWeight: "bold",
+  color: "#cd3545",
+};
 const ContactForm = () => {
   // destructuramos los datos que trae nuestro Hook personalizado
   const {
@@ -29,6 +70,8 @@ const ContactForm = () => {
           value={form.name}
           required
         />
+        {/* si error name contiene información debemos mostralo */}
+        {errors.name && <p style={styles}>{errors.name}</p>}
         <input
           type="email"
           name="email"
@@ -38,6 +81,7 @@ const ContactForm = () => {
           value={form.email}
           required
         />
+        {errors.email && <p style={styles}>{errors.email}</p>}
         <input
           type="text"
           name="subject"
@@ -47,12 +91,20 @@ const ContactForm = () => {
           value={form.subject}
           required
         />
-        <textarea name="comments" cols="50" rows="5" placeholder="Escribe tu comentaroio"  onBlur={handleBlur}
+        {errors.subject && <p style={styles}>{errors.subject}</p>}
+        <textarea
+          name="comments"
+          cols="50"
+          rows="5"
+          placeholder="Escribe tu comentaroio"
+          onBlur={handleBlur}
           onChange={handleChange}
           value={form.comments}
-          required></textarea>
+          required
+        ></textarea>
+        {errors.comments && <p style={styles}>{errors.comments}</p>}
 
-          <input type="submit" value="enviar"/>
+        <input type="submit" value="enviar" />
       </form>
     </div>
   );
